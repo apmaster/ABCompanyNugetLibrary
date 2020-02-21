793b0ce1aeb3ad7d1b807b87c9b2dcb816847675
Andre <andre123us@yahoo.com>
2020-02-21 12:42:58 -0800

    Initial Commit
    Snack Machine Logic source file.

---
+++ SnackMachineLogic/ABCompanyDomain.SnackMachineLogic/ABCompanyDomainSnackMachineLogic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using static ABCompanyDomain.SnackMachineLogic.ABCompanyDomainSnackMachineLogicMoneyBills;
+
+namespace ABCompanyDomain.SnackMachineLogic
+{
+    public class ABCompanyDomainSnackMachineLogic:ABCompanyDomainSnackMachineLogicEntity
+    {
+          public ABCompanyDomainSnackMachineLogicMoneyBills MoneyBillsInside { get; private set; } = None;
+          public ABCompanyDomainSnackMachineLogicMoneyBills MoneyBillsInTransaction { get; private set; } = None;
+
+        public void InsertMoneyBills(ABCompanyDomainSnackMachineLogicMoneyBills DollarBill)
+        {
+            ABCompanyDomainSnackMachineLogicMoneyBills[] AllDollars = { Dollar, FiveDollar, TenDollar, TwentyDollar };
+            if (!AllDollars.Contains(DollarBill))
+            {
+                throw new InvalidOperationException();
+            }
+            MoneyBillsInTransaction += DollarBill;
+        }
+
+        public void ReturnMoney()
+        {
+            MoneyBillsInTransaction = None;
+        }
+
+        public void BuySnack()
+        {
+            MoneyBillsInside += MoneyBillsInTransaction;
+            MoneyBillsInTransaction = None;
+        }
+    }
+}
